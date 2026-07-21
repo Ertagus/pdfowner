49f301ec3c51474ffc0c46f67c45de3a1f521d72
Ertagus <ertagus@gmail.com>
2026-07-21 15:05:19 +0200

    Update pdfowner_it.py

--- pdfowner_it.py
+++ pdfowner_it.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # pdfowner - estrae e cracka le owner password dei PDF
-# Copyright (C) 2026 <nome>
+# Copyright (C) 2026 Ertagus
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
